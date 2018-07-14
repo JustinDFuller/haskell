1a84b2d1a0c611ade43e4942a267148a5085230f
Justin Fuller <justindanielfuller@gmail.com>
2018-07-14 08:32:25 -0400

    Update listComprehension.hs

--- listComprehension.hs
+++ listComprehension.hs
@@ -15,12 +15,31 @@ let firstTenEvenNumbersGreaterThanTwelve = [x*2 | x <- [1..10], x*2 >= 12] -- [1
 -- The predicate filters the output
 
 -- Here's a simple filter function, keeping any number where x divided by 7, the remainder is 3
-let divideBySevenRemainderThree = [ x | x <- [50..100], x `mod` 7 == 3]
+let divideBySevenRemainderThree = [x | x <- [50..100], x `mod` 7 == 3]
 -- [52,59,66,73,80,87,94] 
 
 -- The output function is what many fp programmers would call a map
--- The predicate is what many fp programmers would call a fitler
+-- The predicate is what many fp programmers would call a filter
 
--- You can have multiple filters
-let notThirteenFifteenOrNineteen = [ x | x <- [10..20], x /= 13, x /= 15, x /= 19]  
+-- You can have multiple predicates
+let notThirteenFifteenOrNineteen = [x | x <- [10..20], x /= 13, x /= 15, x /= 19]  
 -- [10,11,12,14,16,17,18,20]  
+
+-- A list comprehension can have multiple lists, and all possible combinations
+-- Will be given to the output function
+let twoListComprehension = [x*y | x <- [2,5,10], y <- [8,10,11]] 
+-- [16,20,22,40,50,55,80,100,110] 
+
+-- You can see 2 was multiplied by 8, 10, and 11. The same goes for 5 and 10.
+
+-- You can still filter with a predicate, just as before
+let twoListCompWithFilter = [x*y | x <- [2,5,10], y <- [8,10,11], x*y > 50]
+-- [55,80,100,110]
+
+-- As you guessed, comprehensions are not limited to numbers
+let nouns = ["hobo","frog","pope"]
+let adjectives = ["lazy","grouchy","scheming"]
+let nounAdjectiveCombos = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]
+-- ["lazy hobo","lazy frog","lazy pope","grouchy hobo","grouchy frog",  
+-- "grouchy pope","scheming hobo","scheming frog","scheming pope"] 
+
