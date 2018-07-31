4eaf25a53d32c044f9b44051a37ad22f43cb13e1
Justin Fuller <justindanielfuller@gmail.com>
2018-07-30 20:40:30 -0400

    Trying to get sql to work

---
+++ sql/postgres/example.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Database.PostgreSQL.Simple
+
+main :: IO Int
+main = do
+  conn <- connectPostgreSQL ""
+  [Only i] <- query_ conn "select 2 + 2"
+  return i
